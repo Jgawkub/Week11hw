4ef60d21954c84aa3e85129d3fffb4e07fcd72d1
Jgawkub <jgaweda@yahoo.com>
2022-10-10 17:13:36 -0400

    saving progress

--- tic.js
+++ tic.js
@@ -1,10 +1,11 @@
 let hide = $('#hide')
 let show = $('#show')
 let test=$('#test')
-console.log(test.text)
+let turns=0
 
-
-
+for(let i=1; i<10;i++){
+    console.log(`it is turn" ${i}`)
+}
 
 
 
@@ -17,4 +18,12 @@ hide.on('click',()=>{
 show.on('click',()=>{
     console.log("yerp")
 $("#test").show()
+})
+
+
+let cell = $('.cell')
+
+
+$('div.cell').on('click',()=>{
+    cell.text("you've clicked a tile!")
 })
