b21365ec1f2b47064f5472271bd92bf038a78f44
Jgawkub <jgaweda@yahoo.com>
2022-10-10 22:47:21 -0400

    more progress

--- tic.js
+++ tic.js
@@ -2,11 +2,33 @@ let hide = $('#hide')
 let show = $('#show')
 let test=$('#test')
 let turns=0
+let reset=$("#reset")
+let cell = $('.cells')
+let player1= "X"
+let player2= "O"
+let currentPlayer=player1
+let swap = $("#switch")
+
+
+
+swap.on('click',()=>{
+    if(currentPlayer===player1){
+        currentPlayer=player2
+        $('#playerTurn').text("0's turn")
+
+    }else if(currentPlayer===player2){
+        currentPlayer=player1
+        $('#playerTurn').text("X's turn")
+    }
+})
+
+
+
+
 
 for(let i=1; i<10;i++){
     console.log(`it is turn" ${i}`)
 }
-
 
 
 
@@ -21,9 +43,18 @@ $("#test").show()
 })
 
 
-let cell = $('.cell')
 
 
-$('div.cell').on('click',()=>{
+reset.on('click',()=>{
+    console.log("is this working")
+   cell.text("")
+})
+
+// for (let i=0; i<cell.length; i++){
+//     console.log(cell[i])
+// }
+
+cell.on('click',()=>{
     cell.text("you've clicked a tile!")
 })
+
